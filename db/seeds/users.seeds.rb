40462770e0cfaade887727bd348f0d5ec3e5437c
deniszackharov <denis.zaharov@flatstack.com>
2022-03-02 01:25:22 +0300

    fix seeds

--- db/seeds/users.seeds.rb
+++ db/seeds/users.seeds.rb
@@ -1,4 +1,4 @@
 print "Creating users... "
 
-  User.create!(first_name: "John", last_name: "Doe", email: "test1@example.com", password: "123456", confirmed_at: Time.now)
+  User.create!(first_name: "John", last_name: "Doe", email: "test1@example.com", password: "123456")
 puts "Done!"
